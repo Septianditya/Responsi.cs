815e26f4d0dfedc60682a2d05a3150f5dc748694
Septianditya <101825691+Septianditya@users.noreply.github.com>
2022-07-16 11:26:12 +0700

    Create Mahasiswa.cs

---
+++ Mahasiswa.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ProjectMahasiswa
+{
+    public class Mahasiswa
+    {
+        // PERINTAH: lengkapi property class mahasiswa, sesuai petunjuk soal
+        public int nim;
+        public string nama;
+        public string Kelamin;
+        public double total;
+    }
+}
